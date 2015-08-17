5ef7c4d91968cf8e7727ef87da2c3238b2ca2513
Mary Kergosien <mary@kergosien.net>
2015-08-17 13:11:35 -0500

    added wish and watched hbd

--- javascripts/main.js
+++ javascripts/main.js
@@ -19,8 +19,8 @@ requirejs.config({
 
 
 
-requirejs( ["jquery", "lodash", "firebase", "hbs", "bootstrap", "getMovies", "addMovies"],
-    function($, _, _firebase, Handlebars, bootstrap, get, add) {
+requirejs( ["jquery", "lodash", "firebase", "hbs", "bootstrap", "getMovies", "addMovies", "bootstrap-rating"],
+    function($, _, _firebase, Handlebars, bootstrap, get, add, rating) {
 
       var tempMovies = {};
       var watchedMovies = {};
@@ -43,24 +43,88 @@ requirejs( ["jquery", "lodash", "firebase", "hbs", "bootstrap", "getMovies", "ad
                 var populatedTemplate = movieTemplate(tempMovies);
                 $("#movie-list").html(populatedTemplate);
               }); 
-            });
-        });
-
-        $("#addToWishlist").click(function(addevt){
-              console.log(addevt);
-              var addFB = $(this).parent().attr('key');
-              add.addMovies(addFB);
-              $(this).attr('disabled','disabled');
 
             require(
               ['hbs!../templates/wishlist'],
               function(movieTemplate){
-                var populatedTemplate = movieTemplate(FBMovie);
+                var populatedTemplate = movieTemplate({movies:FBMovie});
+                console.log(FBMovie)
+                console.log(populatedTemplate)
                 $("#movie-list").append(populatedTemplate);
             });
 
-             $("#titleInput").val('');
+            require(
+              ['hbs!../templates/watched'],
+              function(movieTemplate){
+                var populatedTemplate = movieTemplate({movies:FBMovie});
+                console.log(FBMovie)
+                console.log(populatedTemplate)
+                $("#movie-list").append(populatedTemplate);
+                $('.rating').rating();
+            });
+
+            $("#titleInput").val('');
+          });
         }); 
+
+        $(document).on('click', "#addToWishlist", function(addevt){
+            console.log("click", addevt);
+
+            var addFB = $(this).parent().attr('key');
+            add.addMovie(addFB);
+            $(this).attr('disabled','disabled');
+        });
+
+        $(document).on('click', "#watchedButton", function(addevt){
+            console.log("click", addevt);
+            var watchedKey = $(this).parent().attr("key");
+            var seenIt = new Firebase('https://movie-project.firebaseio.com/movies/' + watchedKey);
+            if ( $(this).parent().attr("watched") === "false" ) {
+                seenIt.update({'seen-it': true});
+              } else {
+                seenIt.update({'seen-it': false});
+              }
+        });
+
+        //Delete Button
+        $( document ).on( "click", "#deleteButton", function() {
+          var titleKey = $(this).parent().attr("key");
+          console.log("titleKey", titleKey);
+          var fb = new Firebase('https://movie-project.firebaseio.com/movies/' + titleKey);
+          fb.remove();
+        });
+
+
+       // // Watched Button
+       //  $('#radio-watched').on('click', function(){
+       //    var movieInput = $("#titleInput").val().toLowerCase();
+       //    $(".movie-info").filter('[watched="false"]').hide();
+       //    $(".movie-info").filter('[watched="true"]').show();
+       //    $(".search-results").hide();
+       //    $(".movie-info").not('[title*="'+ movieInput + '"]').hide();
+       //  });
+
+       //  //Wish Button
+       //  $('#radio-wish').on('click', function(){
+       //    var movieInput = $("#titleInput").val().toLowerCase();
+       //    $(".movie-info").filter('[watched="true"]').hide();
+       //    $(".movie-info").filter('[watched="false"]').show();
+       //    $(".search-results").hide();
+       //    $(".movie-info").not('[title*="'+ movieInput + '"]').hide();
+       //  });
+
+       //  // Add Button
+       //  $('#radio-add').on('click', function(){
+       //    var movieInput = $("#titleInput").val().toLowerCase();
+       //    $(".movie-info").filter('[watched="false"]').hide();
+       //    $(".movie-info").filter('[watched="true"]').hide();
+       //    $(".search-results").show();
+       //  });
+
+        
+
+
+
 
       });
 
