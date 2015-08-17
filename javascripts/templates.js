ae1b04e8af32a49798c2a7288456191d2ba1fd42
Mary Kergosien <mary@kergosien.net>
2015-08-17 11:06:22 -0500

    add deletions

--- javascripts/templates.js
+++
@@ -1,6 +0,0 @@
-define(["hbs!../templates/movie", "bootstrap-rating", "jquery"],
-function(movieTemplate, bsrating, $) {
-  var templateObj = {};
-  templateObj.movie = modalTemplate;
-  return templateObj;
-});
